307ef6de443d31d39be7ecc5c22bfde52235fbe3
Cyxo <paulolol.jeu@gmail.com>
2020-02-29 19:47:40 +0100

    Added command ~today

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ cam_url = "https://my.own.cam.website:port/"
 
 occupied_endpoint = "someone"
 image_endpoint = "image"
+stats_endpoint = "stats"
 
 news_channel = "news"
 cam_channel = "image"
@@ -13,6 +14,10 @@ occupied_news = "There is people"
 empty_news = "There is no one"
 occupied_message = "diff\n+ Yep, there is someone"
 empty_message = "diff\n- Nope, there is no one"
+
+occupation_title = "Today's occupation"
+no_occupation = "Nobody came today"
+now_term = "now"
 
 image_error_role = "diff\n- You have not the right rôle"
 image_error_channel = "diff\n- You are not in the right channel"
